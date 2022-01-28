9e2e5b11f7562c9cc357d33a94b5d9afe74ca570
you-bowen <2756456886@qq.com>
2022-01-28 21:56:42 +0800

    ..

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ import json
 import os
 import random
 import pickle
+from collections import defaultdict
 app = Sanic(__name__)
 CORS(app)
 
 # http://localhost:19887/random_pic
 @app.route('/random_pic', methods=['GET'])
 def func(req):
+    args = defaultdict(str, req.args)
+    
     with open("pic_list.pkl","rb") as f:
         pic_list = pickle.load(f)
     selected = random.choice(pic_list)
     pic_url = f"https://cdn.jsdelivr.net/gh/you-bowen/random_pics_api/random_pics/{selected}"
-    return redirect(f'{pic_url}')
+    
+    return html(f'<img src="{pic_url}" width="100%" />') if args['raw'] else redirect(f'{pic_url}')
 
 
 if __name__ == '__main__':
